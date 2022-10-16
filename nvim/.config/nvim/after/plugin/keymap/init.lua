93bb77a6f3bd2eeef2edb59835e5a0498885e9e3
dni ⚡ <office@dnilabs.com>
2022-10-16 14:48:14 +0200

    git, buffer and window navigation keybinds

--- nvim/.config/nvim/after/plugin/keymap/init.lua
+++ nvim/.config/nvim/after/plugin/keymap/init.lua
@@ -8,8 +8,24 @@ local nmap = Remap.nmap
 nnoremap("<leader>f", ":Ex<CR>")
 nnoremap("<leader><enter>", ":terminal<CR>")
 nnoremap("<leader>t", ":TagbarToggle<CR>")
+
+-- coc binds
 nnoremap("<leader>cc", ":CocCommand<CR>")
+nnoremap("<leader>cr", ":CocReset<CR>")
 nnoremap("<leader>cl", ":CocDiagnostics<CR>")
 nnoremap("<leader>cn", ":CocNext<CR>")
 nnoremap("<leader>cf", ":CocFirst<CR>")
 
+-- navigate through windows
+nnoremap("<C-l>", "<C-w><C-w>")
+nnoremap("<C-h>", "<C-w>h")
+-- navigate through buffers
+nnoremap("<C-k>", ":bnext<cr>")
+nnoremap("<C-j>", ":bprevious<cr>")
+
+-- Fugitive Conflict Resolution
+nnoremap("<leader>gd", ":Gvdiffsplit!<CR>")
+nnoremap("<leader>gdh", ":diffget //2<CR>")
+nnoremap("<leader>gdl", ":diffget //3<CR>")
+nnoremap("<leader>gdu", ":diffupdate<CR>")
+nnoremap("<leader>gdw", ":Gwrite!<CR>")
